9d787687030d04e00463df1f1bcd77cef2f64b8e
Alfredo Pérez <index@mailbox.org>
2025-07-04 18:54:52 -0600

    sys: replace boot and trap assembly with C implementations

---
+++ sys/boot.c
@@ -0,0 +1,175 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <fe310/aon.h>
+#include <fe310/clint.h>
+#include <fe310/gpio.h>
+#include <fe310/plic.h>
+#include <fe310/prci.h>
+#include <fe310/spi.h>
+#include <fe310/uart.h>
+
+#include "hifive1.h"
+
+#define RTCFQ 32768
+#define MAGIC 0xF001BA11
+
+#define MIE_MSIE BITMASK(3)
+#define MIE_MTIE BITMASK(7)
+#define MIE_MEIE BITMASK(11)
+#define MSTATUS_MIE BITMASK(3)
+
+#ifndef UART_BAUDRATE
+#define UART_BAUDRATE 115200
+#endif
+
+#ifndef QSPI_BAUDRATE
+#define QSPI_BAUDRATE 40000000
+#endif
+
+#define TRAP_STACK_SIZE 512
+__attribute__((used, aligned(16)))
+static u8 trap_stack[TRAP_STACK_SIZE];
+
+extern void trap_entry(void);
+extern void _start(void);
+
+static void
+wait(void)
+{
+	u64 next = clint_get_mtime() + RTCFQ / 2 + 1;
+	while (clint_get_mtime() < next);
+}
+
+static inline void
+guard(void)
+{
+	u32 save;
+
+	if (aon->backup[15] == MAGIC) {
+		aon->backup[15] = 0;
+
+		gpio->output_en = LED_R;
+		while (1) {
+			wait();
+			gpio_tgl(LED_R);
+		};
+	}
+
+	save = aon->backup[15];
+
+	gpio->output_en = LED_G;
+	wait();
+	gpio_set(LED_G);
+	aon->backup[15] = MAGIC;
+	wait();
+	aon->backup[15] = save;
+	gpio_clr(LED_G);
+	wait();
+	gpio->output_en = 0;
+}
+
+static inline void
+clock(void)
+{
+	const u32 r = 1, f = 31, q = 1;
+	prci_pll(r, f, q, PRCI_PLL_SEL_HFROSC, PRCI_PLL_REFSEL_HFXOSC, false);
+	while(prci_is_pll_lock());
+	prci_set_pll_sel(PRCI_PLL_SEL_PLL);
+	prci_set_hfrosc_en(false);
+}
+
+static inline void
+flash(u32 hfclk, u32 baud)
+{
+	spi_baudrate(spi0, hfclk, baud);
+	spi_ffmt(spi0, true, 3, 4, SPI_PROTO_S, SPI_PROTO_D, SPI_PROTO_D, 0xBB, 0x00);
+}
+
+static inline void
+tty(u32 hfclk, u32 baud)
+{
+	uart_baudrate(uart0, hfclk, baud);
+	uart_txctrl(uart0, true, UART_STOP_BITS_ONE, WM_0);
+	uart_rxctrl(uart0, true, WM_0);
+	uart_iof(uart0, true, UART_IOF_TXRX);
+}
+
+static inline void
+plic(void)
+{
+	PlicIrq irq;
+	*(volatile u32 *)(PLIC_BASE_IE) = 0;
+	*(volatile u32 *)(PLIC_BASE_IE + 4) = 0;
+	plic_threshold(PLIC_PRIORITY_0);
+	for (irq = PLIC_IRQ_NONE; irq <= PLIC_IRQ_I2C; irq++)
+		plic_priority(irq, PLIC_PRIORITY_0);
+}
+
+static inline void
+riscv(void)
+{
+	__asm__ volatile (
+		"csrw mscratch, %0"
+		:
+		: "r"((u32)trap_stack + TRAP_STACK_SIZE)
+		: "memory"
+	);
+
+	__asm__ volatile (
+		"csrci mstatus, %0"
+		:
+		: "i"(MSTATUS_MIE)
+		: "memory"
+	);
+
+	__asm__ volatile (
+		"csrw mie, zero"
+		:
+		:
+		: "memory"
+	);
+
+	__asm__ volatile (
+		"csrw mtvec, %0"
+		:
+		: "r"(&trap_entry)
+		: "memory"
+	);
+
+	__asm__ volatile (
+		"fence.i"
+		:
+		:
+		: "memory"
+	);
+
+	__asm__ volatile(
+		"csrs mie, %0"
+		:
+		: "r"(MIE_MEIE)
+		: "memory"
+	);
+
+	__asm__ volatile(
+		"csrsi mstatus, %0"
+		:
+		: "i"(MSTATUS_MIE)
+		: "memory"
+	);
+}
+
+void __attribute__((weak))
+boot(void)
+{
+#ifndef WITHOUT_BOOTGUARD
+	guard();
+#endif
+#ifndef WITHOUT_BOOTCFG
+	clock();
+	u32 hfclk = prci_measure_hfclk(3000);
+	flash(hfclk, QSPI_BAUDRATE);
+	tty(hfclk, UART_BAUDRATE);
+#endif
+	plic();
+	riscv();
+}
---
+++ sys/trap.c
@@ -0,0 +1,227 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <stdint.h>
+#include <unistd.h>
+
+#include <fe310/bits.h>
+#include <fe310/clint.h>
+#include <fe310/plic.h>
+
+/* Masks */
+#define MCAUSE_MSK_CAUSE GENMASK(9, 0)
+
+/* Interrupt codes */
+#define IRQ_M_SOFT 3
+#define IRQ_M_TIMER 7
+#define IRQ_M_EXT 11
+
+/* Exception codes */
+#define CAUSE_USER_ECALL 8
+#define CAUSE_MACHINE_ECALL 11
+
+/* Syscalls */
+#define SYSCALL_READ 63
+#define SYSCALL_WRITE 64
+#define SYSCALL_BRK 214
+
+/* Values */
+#define RTCFQ 32768
+
+typedef struct {
+	uint32_t s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
+	uint32_t ra, t0, t1, t2, t3, t4, t5, t6;
+	uint32_t a0, a1, a2, a3, a4, a5, a6, a7;
+	uint32_t pc;
+	uint32_t pad[3];
+} TrapFrame;
+
+extern ssize_t sys_read(int fd, void *buf, size_t count);
+extern ssize_t sys_write(int fd, const void *buf, size_t count);
+extern long sys_brk(unsigned long brk);
+
+void isr_timer(void);
+void isr_plic(void);
+void isr_clic(void);
+void panic(void);
+
+__attribute__((aligned(16))) u8 trap_stack[512];
+
+static void __attribute((used))
+trap_handler(TrapFrame *tf)
+{
+	uint32_t cause, mcause, mepc;
+
+	__asm__ volatile ( "csrr %0, mcause"
+		: "=&r"(mcause)
+		:
+		: "memory"
+	);
+
+	cause = mcause & MCAUSE_MSK_CAUSE;
+
+	if ((s32)mcause < 0) {
+		switch(cause) {
+		case IRQ_M_TIMER:
+			isr_timer();
+			break;
+		case IRQ_M_EXT:
+			isr_plic();
+			break;
+		case IRQ_M_SOFT:
+			isr_clic();
+			break;
+		default:
+			panic();
+		}
+
+		return;
+	}
+
+	switch(cause) {
+	case CAUSE_USER_ECALL:
+	case CAUSE_MACHINE_ECALL:
+		switch(tf->a7) {
+		case SYSCALL_READ:
+			tf->a0 = sys_read(
+				(int32_t)tf->a0,
+				(void *)tf->a1,
+				(size_t)tf->a2
+			);
+			break;
+		case SYSCALL_WRITE:
+			tf->a0 = sys_write(
+				(int32_t)tf->a0,
+				(const void*)tf->a1,
+				(size_t)tf->a2
+			);
+			break;
+		case SYSCALL_BRK:
+			tf->a0 = sys_brk((unsigned long)tf->a0);
+			break;
+		default:
+			tf->a0 = -38;
+			break;
+		}
+
+		__asm__ volatile (
+			"csrr %0, mepc\n\t"
+			"addi %0, %0, 4\n\t"
+			"csrw mepc, %0"
+			: "=&r"(mepc)
+			:
+			: "memory"
+		);
+
+		break;
+	default:
+		panic();
+	}
+}
+
+void __attribute__((interrupt, aligned(64)))
+trap_entry(void)
+{
+	__asm__ volatile (
+		"csrrw sp, mscratch, sp\n\t"
+
+		"addi sp, sp, -0x80\n\t"
+		"sw s0, 0x00(sp)\n\t"
+		"sw s1, 0x04(sp)\n\t"
+		"sw s2, 0x08(sp)\n\t"
+		"sw s3, 0x0C(sp)\n\t"
+		"sw s4, 0x10(sp)\n\t"
+		"sw s5, 0x14(sp)\n\t"
+		"sw s6, 0x18(sp)\n\t"
+		"sw s7, 0x1C(sp)\n\t"
+		"sw s8, 0x20(sp)\n\t"
+		"sw s9, 0x24(sp)\n\t"
+		"sw s10, 0x28(sp)\n"
+		"sw s11, 0x2C(sp)\n"
+		"sw ra, 0x30(sp)\n\t"
+		"sw t0, 0x34(sp)\n\t"
+		"sw t1, 0x38(sp)\n\t"
+		"sw t2, 0x3C(sp)\n\t"
+		"sw t3, 0x40(sp)\n\t"
+		"sw t4, 0x44(sp)\n\t"
+		"sw t5, 0x48(sp)\n\t"
+		"sw t6, 0x4C(sp)\n\t"
+		"sw a0, 0x50(sp)\n\t"
+		"sw a1, 0x54(sp)\n\t"
+		"sw a2, 0x58(sp)\n\t"
+		"sw a3, 0x5C(sp)\n\t"
+		"sw a4, 0x60(sp)\n\t"
+		"sw a5, 0x64(sp)\n\t"
+		"sw a6, 0x68(sp)\n\t"
+		"sw a7, 0x6C(sp)\n\t"
+
+		"mv a0, sp\n\t"
+		"call trap_handler\n\t"
+
+		"lw s0, 0x00(sp)\n\t"
+		"lw s1, 0x04(sp)\n\t"
+		"lw s2, 0x08(sp)\n\t"
+		"lw s3, 0x0C(sp)\n\t"
+		"lw s4, 0x10(sp)\n\t"
+		"lw s5, 0x14(sp)\n\t"
+		"lw s6, 0x18(sp)\n\t"
+		"lw s7, 0x1C(sp)\n\t"
+		"lw s8, 0x20(sp)\n\t"
+		"lw s9, 0x24(sp)\n\t"
+		"lw s10, 0x28(sp)\n\t"
+		"lw s11, 0x2C(sp)\n\t"
+		"lw ra, 0x30(sp)\n\t"
+		"lw t0, 0x34(sp)\n\t"
+		"lw t1, 0x38(sp)\n\t"
+		"lw t2, 0x3C(sp)\n\t"
+		"lw t3, 0x40(sp)\n\t"
+		"lw t4, 0x44(sp)\n\t"
+		"lw t5, 0x48(sp)\n\t"
+		"lw t6, 0x4C(sp)\n\t"
+		"lw a0, 0x50(sp)\n\t"
+		"lw a1, 0x54(sp)\n\t"
+		"lw a2, 0x58(sp)\n\t"
+		"lw a3, 0x5C(sp)\n\t"
+		"lw a4, 0x60(sp)\n\t"
+		"lw a5, 0x64(sp)\n\t"
+		"lw a6, 0x68(sp)\n\t"
+		"lw a7, 0x6C(sp)\n\t"
+		"addi sp, sp, 0x80\n\t"
+
+		"csrrw sp, mscratch, sp"
+		:
+		:
+		: "memory"
+	);
+}
+
+void __attribute__((weak))
+isr_timer(void)
+{
+	u64 time = clint_get_mtime();
+	clint_set_mtimecmp(time + RTCFQ);
+}
+
+void __attribute__((weak))
+isr_plic(void)
+{
+	plic_dispatch();
+}
+
+void __attribute__((weak))
+isr_clic(void)
+{
+	clint_msip(false);
+}
+
+void __attribute__((noreturn))
+panic(void)
+{
+	for (;;) {
+		__asm__ volatile (
+			"csrr t0, mcause\n"
+			"csrr t1, mepc\n"
+			"csrr t2, mtval\n"
+			:::
+		);
+	}
+}
